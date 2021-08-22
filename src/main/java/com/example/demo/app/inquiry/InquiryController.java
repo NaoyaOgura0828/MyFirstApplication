006b0f46d3aa1f40801fb5c58f133148b05c9709
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-08-22 21:48:58 +0900

    完了処理の追加

--- src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -5,8 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/inquiry")
@@ -14,7 +16,9 @@ public class InquiryController {
 
     /* フォームの入力値を取得する */
     @GetMapping("/form")
-    public String form(InquiryForm inquiryForm, Model model) {
+    public String form(InquiryForm inquiryForm,
+                       Model model,
+                       @ModelAttribute("complete") String complete) {
         model.addAttribute("title", "Inquiry Form");
         return "inquiry/form";
     }
@@ -38,5 +42,18 @@ public class InquiryController {
         /* 入力内容を表示する（confirm.htmlへ遷移） */
         model.addAttribute("title", "Confirm Page");
         return "inquiry/confirm";
+    }
+
+    @PostMapping("/complete")
+    public String complete(@Validated InquiryForm inquiryForm,
+                           BindingResult result,
+                           Model model,
+                           RedirectAttributes redirectAttributes) {
+        if(result.hasErrors()) {
+            model.addAttribute("title", "Inquiry Form");
+            return "inquiry/form";
+        }
+        redirectAttributes.addFlashAttribute("complete", "Registered!");
+        return "redirect:/inquiry/form"; // ※HTMLファイルではなくURLを指している
     }
 }
