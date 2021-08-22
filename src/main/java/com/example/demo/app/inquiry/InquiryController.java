55d42e8bf83eb0f86efba0877bc719aac7c970c4
SyatyoAdmiral <naoya_ogura@icloud.com>
2021-08-22 20:55:59 +0900

    InquiryController.javaの追記

--- src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -2,7 +2,10 @@ package com.example.demo.app.inquiry;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,5 +16,19 @@ public class InquiryController {
     public String form(InquiryForm inquiryForm, Model model) {
         model.addAttribute("title", "Inquiry Form");
         return "inquiry/form";
+    }
+
+    @PostMapping("/confirm")
+    public String confirm(@Validated InquiryForm inquiryForm,
+                          BindingResult result,
+                          Model model) {
+        /* 入力内容にエラーがあった場合はフォームに戻す（入力内容は保持） */
+        if (result.hasErrors()) {
+            model.addAttribute("title", "Inquiry Form");
+            return "inquiry/form";
+        }
+        /* 確認ページに遷移。入力内容を表示する */
+        model.addAttribute("title", "Confirm Page");
+        return "inquiry/confirm";
     }
 }
